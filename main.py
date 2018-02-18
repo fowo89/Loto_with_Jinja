891947530d055bc0a97ceeb86d7d1166809f7b45
Tomaz <tomo.rifl@gmail.com>
2018-02-18 15:29:38 +0100

    Version1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import jinja2
 import webapp2
+import random
 
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
@@ -31,6 +32,26 @@ class MainHandler(BaseHandler):
     def get(self):
         return self.render_template("hello.html")
 
+class LotoNumbers(BaseHandler):
+    def get(self):
+        numbers = generacija_stevil(7)
+
+        params = {"number1": numbers[0], "number2": numbers[1], "number3": numbers[2], "number4": numbers[3], "number5": numbers[4], "number6": numbers[5], "number7": numbers[6]}
+
+        return self.render_template("loto_generator.html", params=params)
+
+def generacija_stevil(izbira_stevil):
+    seznam_stevil = []
+    for it in range(0, izbira_stevil):
+        while True:
+            random_stevilo = random.randint(1,30)
+            if random_stevilo not in seznam_stevil:
+                break
+        seznam_stevil.append(random_stevilo)
+    return seznam_stevil
+
+
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
+    webapp2.Route('/loto_generator', LotoNumbers),
 ], debug=True)
